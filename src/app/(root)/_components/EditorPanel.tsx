c74a3696c85637e8d6eedf48d6279a6e611a2521
AvikRay1001 <avikray1001@gmail.com>
2025-01-15 20:03:15 +0530

    Final

--- src/app/(root)/_components/EditorPanel.tsx
+++ src/app/(root)/_components/EditorPanel.tsx
@@ -12,6 +12,7 @@ import { EditorPanelSkeleton } from "./EditorPanelSkeleton";
 import useMounted from "@/hooks/useMounted";
 import ShareSnippetDialog from "./ShareSnippetDialog";
 
+
 function EditorPanel() {
   const clerk = useClerk();
   const [isShareDialogOpen, setisShareDialogOpen] = useState(false);
