c7fd241c65d36632a86f9290dfcf9bc09fc52733
AvikRay1001 <avikray1001@gmail.com>
2025-02-08 14:28:08 +0530

    Done

--- src/components/NavigationHeader.tsx
+++ src/components/NavigationHeader.tsx
@@ -62,10 +62,10 @@ function NavigationHeader() {
                 to-orange-500/10 hover:from-amber-500/20 hover:to-orange-500/20 transition-all 
                 duration-300"
               >
-                <Sparkles className="w-4 h-4 text-amber-400 hover:text-amber-300" />
+                {/* <Sparkles className="w-4 h-4 text-amber-400 hover:text-amber-300" />
                 <span className="text-sm font-medium text-amber-400/90 hover:text-amber-300">
                   Pro
-                </span>
+                </span> */}
               </Link>
             </SignedOut>
 
